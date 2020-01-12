7a22c7e3d60a1fd4fc7c16ad07f01508a05cf364
Josh Bryson <josh@bryson.me.uk>
2020-01-12 15:55:20 +0000

    Initial translation of WrapperExample and navbar

---
+++ src/pages/WrapperPage.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import ChartWrapper from '../components/WrapperExample/ChartWrapper/ChartWrapper';
+
+function WrapperPage() {
+  return (
+    <div className="App">
+        <h2>Basic React Wrapper Example</h2>
+        <ChartWrapper />
+        <p>Simple example – examine codebase for further detail</p>
+    </div>
+  );
+}
+
+export default WrapperPage;

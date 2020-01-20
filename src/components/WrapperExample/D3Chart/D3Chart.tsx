05b4c0b5d356c62c9cb32f930c8f305a79622084
Josh Bryson <josh@bryson.me.uk>
2020-01-20 23:07:33 +0000

    Intital implementation of TypeScript in D3 file

--- src/components/WrapperExample/D3Chart/D3Chart.tsx
+++ src/components/WrapperExample/D3Chart/D3Chart.tsx
@@ -1,24 +1,29 @@
 import * as d3 from "d3";
 
+export interface AgesData {
+    age: number,
+    name: string
+}
+
 const url ='https://udemy-react-d3.firebaseio.com/ages.json';
 const width = 500;
 const height = 200;
 
 export default class D3Chart {
-    constructor(element) {
+    constructor(element: any) {
         const svg = d3.select(element)
             .append('svg')
                 .attr('width', width)
                 .attr('height', height)
 
-        d3.json(url).then(agesData => {
+        d3.json(url).then((agesData: Array<AgesData>) => {
             const y = d3.scaleLinear()
-                .domain([0, d3.max(agesData, d => d.age)])
+                .domain([0, d3.max(agesData, (d: any) => d.age)])
                 .range([0, height / 2])
             console.log(agesData);
 
             const x = d3.scaleBand()
-                .domain(agesData.map(d => d.name))
+                .domain(agesData.map((d: AgesData) => d.name))
                 .range([0, width])
                 .padding(0.4)
 
@@ -27,11 +32,11 @@ export default class D3Chart {
 
             rects.enter()
                 .append('rect')
-                    .attr('x', d => x(d.name))
-                    .attr('y', d => height - y(d.age))
+                    .attr('x', (d): any => x(d.name))
+                    .attr('y', (d: AgesData) => height - y(d.age))
                     .attr('width', x.bandwidth)
-                    .attr('height', d => y(d.age))
-                    .attr('fill', d => {
+                    .attr('height', (d: AgesData) => y(d.age))
+                    .attr('fill', (d: AgesData) => {
                         if (d.age > 10){
                             return 'red'
                         }
